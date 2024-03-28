be6d1c8d43d4f5bee7c0420628788dd167102c37
Mariia Mindra <Mariya.Mindra@gmail.com>
2024-03-28 14:01:41 +0100

    hw1 Mindra
    alert and prompt

---
+++ hw1/index.js
@@ -0,0 +1,6 @@
+alert('Hello!');
+let userName = prompt('What is your name?', 'user');
+let userCountry = prompt('Where are you from?'+userName, 'Ukraine');
+let resultUserInfo = 'Nice to meet you, ' + userName + '! You are from ' + userCountry + '!';
+console.log(resultUserInfo);
+alert(resultUserInfo);
