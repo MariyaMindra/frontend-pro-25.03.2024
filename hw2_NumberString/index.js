d2529c86951ac7fa3906f1a132897343c758e17e
Mariia Mindra <Mariya.Mindra@gmail.com>
2024-04-04 10:59:22 +0200

    hw2
    String concatenation

---
+++ hw2_NumberString/index.js
@@ -0,0 +1,6 @@
+let userString1 = prompt('Enter string:', 'Hello');
+let userString2 = prompt('Enter second string:', 'Name');
+let userString3 = prompt('Enter third string:', 'Country');
+
+let finalString = userString1 +' '+userString2 +' '+ userString3;
+console.log('Your strings: ' + finalString);
