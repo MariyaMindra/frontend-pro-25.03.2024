f5da13786b222a1ab3252b9fb4a1ce1a58e99bd2
Mariia Mindra <Mariya.Mindra@gmail.com>
2024-04-04 12:10:07 +0200

    hw2_Mindra_DataTypes
    JS data types

---
+++ hw3_DataTypes/index.js
@@ -0,0 +1,14 @@
+console.log('Primitive values: ')
+console.log(typeof undefined);
+console.log(typeof true);
+console.log(typeof 'Hello');
+console.log(typeof 1);
+console.log(typeof 10n);
+console.log(typeof NaN);
+console.log(typeof Symbol('id'));
+console.log('Mutable values: ');
+console.log(typeof Math);
+console.log(typeof {name:'Mariia', color:'green'});
+console.log(typeof [1,2,3]);
+console.log(typeof alert);
+console.log(typeof null);
