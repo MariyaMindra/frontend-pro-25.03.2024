ca6cddef1f5a97106087e47a412e3c9c9c6ae187
Mariia Mindra <Mariya.Mindra@gmail.com>
2024-04-04 12:34:07 +0200

    hw4_Mindra_Number

---
+++ hw4_Number/index.js
@@ -0,0 +1,11 @@
+let number = 12369;
+let strNumber = number.toString();
+// let resultStrNumber = strNumber[0]+' '+ strNumber[1] + ' '+ strNumber[2] + ' '+ strNumber[3] + ' '+ strNumber[4];
+// console.log (resultStrNumber);
+
+let digit0 = Number (strNumber[0]);
+let digit1 = Number (strNumber[1]);
+let digit2 = Number (strNumber[2]);
+let digit3 = Number (strNumber[3]);
+let digit4 = Number (strNumber[4]);
+console.log (digit0 + ' ' + digit1 + ' ' + digit2 + ' ' + digit3 + ' ' + digit4 + ' ');
