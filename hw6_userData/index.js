2b03d2b856aba5c124e80b3eff9027566640b2be
Mariia Mindra <Mariya.Mindra@gmail.com>
2024-04-10 11:51:36 +0200

    change code writing, use strict equality, fix error in switch block
    use if structure to check empty string or null.

--- hw6_userData/index.js
+++ hw6_userData/index.js
@@ -2,39 +2,57 @@ const userYearOfBirth = prompt('Enter your year of birth:');
 const userCity = prompt('Enter your city of residence:');
 const userFavoriteSport = prompt('Enter your favorite sport:');
 
-if (userYearOfBirth === null && userCity === null && userFavoriteSport === null)
-{
+if (
+    userYearOfBirth === null &&
+    userCity === null &&
+    userFavoriteSport === null
+){
     alert('Sorry, but you did not provide any information. Reload the page.');
 } else {
     let userAgeMessage;
-    if (userYearOfBirth != null && userYearOfBirth.length == 4 && !isNaN(parseInt(userYearOfBirth))) {
+    if (
+        userYearOfBirth !== null &&
+        userYearOfBirth.length === 4 &&
+        !isNaN(parseInt(userYearOfBirth))
+    ) {
         const userAge = 2024 - userYearOfBirth;
         userAgeMessage = 'Your age is: ' + userAge;
-    } else userAgeMessage = 'Sorry, but you did not provide information about year of birth.';
+    } else
+        userAgeMessage = 'Sorry, but you did not provide information about year of birth';
     let userCityMessage;
-    if (userCity != null && userCity.length != 0) {
+    if (userCity !== null && userCity.trim() !== '')
+    {
         userCityMessage ='You live in the city '+userCity;
-        if (userCity === 'Kyiv' || userCity === 'Washington' || userCity === 'London'){
-            userCityMessage = 'You live in the capital ' + userCity;}
-    } else userCityMessage = 'Sorry, but you did not provide information about your city.';
+        if (
+            userCity === 'Kyiv' ||
+            userCity === 'Washington' ||
+            userCity === 'London'
+        ){
+            userCityMessage = 'You live in the capital ' + userCity;
+        }
+    } else
+        userCityMessage = 'Sorry, but you did not provide information about your city';
 
     let sportMessage = 'Your favorite sport is ' + userFavoriteSport;
-    switch (userFavoriteSport) {
-        case null || userFavoriteSport.length == 0: {
-            sportMessage = 'Sorry, but you did not provide information about sport.';
-            break;
-        }
-        case 'football': {
-            sportMessage += ' Cool! Want to be like Cristiano Ronaldo.';
-            break;
-        }
-        case 'basketball': {
-            sportMessage += ' Cool! Want to be like Michael Jordan.';
-            break;
-        }
-        case 'tennis': {
-            sportMessage += ' Cool! Want to be like Novak Djokovic.';
-            break;
+    if (userFavoriteSport === null || userFavoriteSport.trim() === '')
+    {
+        sportMessage = 'Sorry, but you did not provide information about sport.';
+    } else
+    {
+        switch (userFavoriteSport)
+        {
+            case 'football': {
+                sportMessage += ' Cool! Want to be like Cristiano Ronaldo.';
+                break;
+            }
+            case 'basketball': {
+                sportMessage += ' Cool! Want to be like Michael Jordan.';
+                break;
+            }
+            case 'tennis': {
+                sportMessage += ' Cool! Want to be like Novak Djokovic.';
+                break;
+            }
         }
     }
     alert(userAgeMessage + '.\n' + userCityMessage + '.\n' + sportMessage);
