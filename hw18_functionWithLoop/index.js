c3c488cb1b8aa049151945ca3fb02896dd5501d6
Mariia Mindra <Mariya.Mindra@gmail.com>
2024-04-18 17:00:40 +0200

    hw18_Mindra_function with loop

---
+++ hw18_functionWithLoop/index.js
@@ -0,0 +1,32 @@
+function askForNumber(cb){
+    let userNumber;
+    for (let i = 0; i <= 10; i++)
+    {
+        userNumber = prompt ('Enter a number greater than 100: ');
+        if (userNumber !== null && +userNumber > 100)
+        {
+            return cb(userNumber);
+        }
+    }
+    return cb(null);
+}
+askForNumber(function(number) {
+    if (number !== null)
+    {
+        console.log('You enter a number: ', number);
+    } else
+    {
+        console.log('Not valid number.');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
